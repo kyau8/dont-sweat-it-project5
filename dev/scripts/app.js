7f70d3cd7c44e75f718c45892513b7fa23a3b732
Kitty <kittyyau8@gmail.com>
2017-11-22 16:58:45 -0500

    Updating Scripts

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import taskObjects from './taskObjects';
 
 // PSEUDOCODE
 // Overview: create a 'done it' app to mark down the simple tasks that have been completed, with the goal of allowing users to keep track of what has/has not been done
@@ -8,3 +9,23 @@ import ReactDOM from 'react-dom';
 // Users can mark tasks as completed, not-completed, or ??? (forgotten) - default is neutral. This information will be saved to Firebase.
 // Users can select 'new day' to save data for a new day/week/month. The previous day/week/month's information will be available for access (still stored in Firebase).
 // Stretch goals: user authentication, and adding dates into the app so that the information automatically refreshes (use the date object, and add a 'created' property to the task object)
+
+// Create an app component that prints ta
+class App extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+            // Users will push the tasks of their choosing into these three arrays, to display those tasks on the page
+            dailyTasks: [],
+            weeklyTasks: [],
+            monthlyTasks: []
+        }
+    }
+    render() {
+        return (
+            <h1>Meep</h1>
+        )
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
---
+++ dev/scripts/taskObjects.js
@@ -0,0 +1,235 @@
+import React from 'react';
+
+class TaskObjects extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            userTasks: {
+                lockCar: {
+                    task: 'Locked the car',
+                    // dateCreated:
+                    taskStatus: [
+                        'Default',
+                        'Completed',
+                        'Nope',
+                        "Can't remember"
+                    ]
+                },
+                lockHouse: {
+                    task: 'Locked the front door',
+                    // dateCreated:
+                    taskStatus: [
+                        'Default',
+                        'Completed',
+                        'Nope',
+                        "Can't remember"
+                    ]
+                },
+                closeGarage: {
+                    task: 'Closed the garage door',
+                    // dateCreated:
+                    taskStatus: [
+                        'Default',
+                        'Completed',
+                        'Nope',
+                        "Can't remember"
+                    ]
+                },
+                bringKeys: {
+                    task: 'Brought my keys',
+                    // dateCreated:
+                    taskStatus: [
+                        'Default',
+                        'Completed',
+                        'Nope',
+                        "Can't remember"
+                    ]
+                },
+                bringPhone: {
+                    task: 'Brought my phone',
+                    // dateCreated:
+                    taskStatus: [
+                        'Default',
+                        'Completed',
+                        'Nope',
+                        "Can't remember"
+                    ]
+                },
+                bringwallet: {
+                    task: 'Brought my wallet',
+                    // dateCreated:
+                    taskStatus: [
+                        'Default',
+                        'Completed',
+                        'Nope',
+                        "Can't remember"
+                    ]
+                },
+                bringUmbrella: {
+                    task: 'Brought my umbrella',
+                    // dateCreated:
+                    taskStatus: [
+                        'Default',
+                        'Completed',
+                        'Nope',
+                        "Can't remember"
+                    ]
+                },
+                bringTransitPass: {
+                    task: 'Brought my transit pass',
+                    // dateCreated:
+                    taskStatus: [
+                        'Default',
+                        'Completed',
+                        'Nope',
+                        "Can't remember"
+                    ]
+                },
+                lightsOff: {
+                    task: 'Turned the lights off',
+                    // dateCreated:
+                    taskStatus: [
+                        'Default',
+                        'Completed',
+                        'Nope',
+                        "Can't remember"
+                    ]
+                },
+                chargePhone: {
+                    task: 'Charged my phone',
+                    // dateCreated:
+                    taskStatus: [
+                        'Default',
+                        'Completed',
+                        'Nope',
+                        "Can't remember"
+                    ]
+                },
+                defrostingMeat: {
+                    task: 'Put the meat out to defrost',
+                    // dateCreated:
+                    taskStatus: [
+                        'Default',
+                        'Completed',
+                        'Nope',
+                        "Can't remember"
+                    ]
+                },
+                wateringPlants: {
+                    task: 'Watered the plants',
+                    // dateCreated:
+                    taskStatus: [
+                        'Default',
+                        'Completed',
+                        'Nope',
+                        "Can't remember"
+                    ]
+                },
+                takeMedicine: {
+                    task: 'Took your medicine',
+                    // dateCreated:
+                    taskStatus: [
+                        'Default',
+                        'Completed',
+                        'Nope',
+                        "Can't remember"
+                    ]
+                },
+                feedPet: {
+                    task: 'Fed my pet',
+                    // dateCreated:
+                    taskStatus: [
+                        'Default',
+                        'Completed',
+                        'Nope',
+                        "Can't remember"
+                    ]
+                },
+                cleanBathroom: {
+                    task: 'Cleaned the bathroom',
+                    // dateCreated:
+                    taskStatus: [
+                        'Default',
+                        'Completed',
+                        'Nope',
+                        "Can't remember"
+                    ]
+                },
+                shovelSnow: {
+                    task: 'Shoveled the snow',
+                    // dateCreated:
+                    taskStatus: [
+                        'Default',
+                        'Completed',
+                        'Nope',
+                        "Can't remember"
+                    ]
+                },
+                getMail: {
+                    task: 'Got the mail',
+                    // dateCreated:
+                    taskStatus: [
+                        'Default',
+                        'Completed',
+                        'Nope',
+                        "Can't remember"
+                    ]
+                },
+                respondTexts: {
+                    task: 'Responded to a text',
+                    taskStatus: [
+                        'Default',
+                        'Completed',
+                        'Nope',
+                        "Can't remember"
+                    ]
+                },
+                loadTransitPass: {
+                    task: 'Loaded your transit pass',
+                    taskStatus: [
+                        'Default',
+                        'Completed',
+                        'Nope',
+                        "Can't remember"
+                    ]
+                },
+                groceryShopped: {
+                    task: 'Went grocery shopping',
+                    taskStatus: [
+                        'Default',
+                        'Completed',
+                        'Nope',
+                        "Can't remember"
+                    ]
+                },
+                didLaundry: {
+                    task: 'Did your laundry',
+                    taskStatus: [
+                        'Default',
+                        'Completed',
+                        'Nope',
+                        "Can't remember"
+                    ]
+                },
+                putOutGarbage: {
+                    task: 'Took the garbage out',
+                    taskStatus: [
+                        'Default',
+                        'Completed',
+                        'Nope',
+                        "Can't remember"
+                    ]
+                },
+                respondTexts: {
+                    task: 'Responded to a text',
+                    taskStatus: [
+                        'Default',
+                        'Completed',
+                        'Nope',
+                        "Can't remember"
+                    ]
+                },
+            }
+        }
+    }
+}
